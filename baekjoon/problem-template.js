f681ad01c788357711a790b13afff7e474ca01b5
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-08-22 16:56:31 +0900

    :tada: solve baekjoon-13225 problem

---
+++ baekjoon/problem-template.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let count = input[0];
+
+for (let i = 1; i < count; i++) {
+    let num = Number(input[i]);
+    var divisorsCount = 0;
+    for (let j = 1; j <= num; j++) {
+        if (num % j === 0) {
+            divisorsCount++;
+        }
+    }
+    console.log(num + ' ' + divisorsCount + '\n');
+}
