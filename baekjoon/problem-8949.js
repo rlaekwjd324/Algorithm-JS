f859b26597b79154ac8dda235389983f239490c7
KimDaJung <ekwjd324@gmail.com>
2022-08-27 22:06:39 +0900

    :zap: solve other solution

--- baekjoon/problem-8949.js
+++ baekjoon/problem-8949.js
@@ -1,14 +1,15 @@
-let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
-let input = fs.readFileSync('test.txt').toString().trim().split(' ');
-
-let firstNumber = input[0];
-let secondNumber = input[1];
-firstNumber = '0'.repeat(Math.max(firstNumber.length, secondNumber.length) - firstNumber.length) + firstNumber;
-secondNumber = '0'.repeat(Math.max(firstNumber.length, secondNumber.length) - secondNumber.length) + secondNumber;
+let input =
+    require('fs').readFileSync('test.txt').toString().trim().split(' ').map(function (e) {
+        return e.split('').map(function (e) {
+            return parseInt(e);
+        });
+    });
 var sum = '';
-
-for (let i = 0; i < firstNumber.length; i++) {
-    sum += (parseInt(firstNumber[i]) + parseInt(secondNumber[i]));
+while (input[0].length > 0 || input[1].length > 0) {
+    var a = input[0].pop();
+    var b = input[1].pop();
+    a = (a === undefined ? 0 : a);
+    b = (b === undefined ? 0 : b);
+    sum = (a + b) + sum;
 }
 console.log(sum);
