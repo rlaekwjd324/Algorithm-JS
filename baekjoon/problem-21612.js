7e695719ca16feff66d3668fd93e08a73230e441
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-09-06 11:13:18 +0900

    :tada: solve baekjoon-21612

--- baekjoon/problem-21612.js
+++ baekjoon/problem-21612.js
@@ -5,4 +5,4 @@ let input = fs.readFileSync('test.txt').toString().trim().split('\n');
 let number = parseInt(input[0]);
 let p = number * 5 - 400;
 console.log(p);
-console.log(p >= number ? (p === 0 ? '0' : '-1') : '1');
+console.log(p >= 100 ? (p === 100 ? '0' : '-1') : '1');
