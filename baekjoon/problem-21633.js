1e175d3777f95ca7edc99a1ded5e67b3871173cb
rlaekwjd324 <rew324@naver.com>
2022-10-07 12:35:10 +0900

    change

--- baekjoon/problem-21633.js
+++ baekjoon/problem-21633.js
@@ -4,4 +4,4 @@ let num = parseInt(input[0]) * 0.01 + 25;
 const temp = num * 100;
 const temp2 = Math.floor(temp);
 const result = (temp2 / 100).toFixed(2);
-console.log(result > 2000 ? '2000.00' : (result >= 100 ? result : '100.00')); 
+console.log(result > 2000 ? '2000.00' : (result >= 100 ? result : '100.00'));
