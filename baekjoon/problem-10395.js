70591526b317c00a26e5ddc7557f8be3c5f6e527
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-10-21 11:52:22 +0900

    :tada: solve baekjoon-10395

---
+++ baekjoon/problem-10395.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync('test.txt').toString().trim().split('\n');
+let firstLine = input[0].split(' ');
+let secondLine = input[1].split(' ');
+for (let i = 0; i < 5; i++) {
+    if (firstLine[i] === '0' && secondLine[i] === '0') {
+        console.log('N');
+        return;
+    }
+}
+console.log('Y');
