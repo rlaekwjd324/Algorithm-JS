74d63c2b0f911e3d6de91a57ac4a9216f068117e
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-10-25 13:58:58 +0900

    :tada: solve baekjoon-11522

---
+++ baekjoon/problem-11522.js
@@ -0,0 +1,48 @@
+// let fs = require('fs');
+// // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let input = fs.readFileSync('test.txt').toString().trim().split('\n');
+// let answer = '';
+// for (let i = 1; i <= parseInt(input[0]); i++) {
+//     let line = input[i].split(' ').map(function (v) { return parseInt(v); });
+//     answer += (line[0].toString() + ' ');
+//     let s1 = 0;
+//     let s2 = 0;
+//     let s3 = 0;
+//     for (let j = 1; j <= line[1]; j++) {
+//         s1 += j;
+//         s2 += (j * 2 - 1);
+//         s3 += (j * 2);
+//     }
+//     answer += (s1 + ' ' + s2 + ' ' + s3 + '\n');
+// }
+// console.log(answer.trim());
+
+// 예제 입력이 여러줄로 되어 있을 떼
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+rl.on('line', function (line) {
+    input.push(line);
+}).on('close', function () {
+    let answer = '';
+    for (let i = 1; i <= parseInt(input[0]); i++) {
+        let line = input[i].split(' ').map(function (v) { return parseInt(v); });
+        answer += (line[0] + ' ');
+        let s1 = 0;
+        let s2 = 0;
+        let s3 = 0;
+        for (let j = 1; j <= line[1]; j++) {
+            s1 += j;
+            s2 += (j * 2 - 1);
+            s3 += (j * 2);
+        }
+        answer += (s1 + ' ' + s2 + ' ' + s3 + '\n');
+    }
+    console.log(answer.trim());
+    process.exit();
+});
