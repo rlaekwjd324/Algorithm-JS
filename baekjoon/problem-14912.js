975a25b3a1c4322a1deaf1119048245379d458b2
rlaekwjd324 <rew324@naver.com>
2022-11-05 12:43:23 +0900

    :tada: solve baekjoon-14912 by that case

--- baekjoon/problem-14912.js
+++ baekjoon/problem-14912.js
@@ -1,11 +1,13 @@
 let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(function (value) { return parseInt(value); });
 let input = fs.readFileSync('test.txt').toString().trim().split(' ').map(function (value) { return parseInt(value); });
-let digitCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let digitCount = 0;
 for (let i = 1; i <= input[0]; i++) {
     let word = i.toString().split('');
     for (let j = 0; j < word.length; j++) {
-        digitCount[parseInt(word[j])]++;
+        if (word[j] === input[1].toString()) {
+            digitCount++
+        }
     }
 }
-console.log(digitCount[input[1]]);
+console.log(digitCount);
