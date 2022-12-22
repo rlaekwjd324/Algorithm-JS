5f043a5fef0ab50e8f1ce8e8cd233a7ac41a2b1c
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2022-12-22 11:45:31 +0900

    :tada: solve baekjoon-11576

--- baekjoon/problem-11576.js
+++ baekjoon/problem-11576.js
@@ -1,19 +1,19 @@
-let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-let input = fs.readFileSync('test.txt').toString().trim().split('\n');
-let A = parseInt(input[0].split(' ')[0]);
-let B = parseInt(input[0].split(' ')[1]);
-let m = parseInt(input[1]);
-let number = input[2].split(' ').map(function (value) {
-    return parseInt(value);
-});
-let realNumber = 0;
-for (let i = 0; i < m; i++) {
-    realNumber += (Math.pow(A, i) * number[number.length - i - 1]);
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = parseInt(input[0].split(' ')[0])
+let x = parseInt(input[0].split(' ')[1])
+let answer = 1
+let voices = input[1].split(' ').map(function (v) { return parseInt(v)})
+if (x <= voices[0]) {
+  let i = 0
+  while (true) {
+    i = (i + 1) % n
+    if (x >= voices[i]) {
+      answer = i + 1
+      break
+    }
+    x = x + 1
+  }
 }
-let answerNumber = '';
-while (realNumber > 0) {
-    answerNumber = (realNumber % B) + ' ' + answerNumber;
-    realNumber = parseInt(realNumber / B);
-}
-console.log(answerNumber);
+console.log(answer)
