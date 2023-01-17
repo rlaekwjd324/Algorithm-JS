474e2f78a0086ab01a892f521a0bc9af2b1a8edf
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-01-17 19:15:30 +0900

    :tada: solve baekjoon-23746

---
+++ baekjoon/problem-23746.js
@@ -0,0 +1,17 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = parseInt(input[0])
+let keys = new Array(n)
+let values = new Array(n)
+for (let i = 1; i <= n; i++) {
+  let line = input[i].split(' ')
+  keys[i - 1] = line[0]
+  values[i - 1] = line[1]
+}
+let word = input[n + 1].split('')
+let preWord = ''
+for (let i = 0; i < word.length; i++) {
+  preWord += keys[values.indexOf(word[i])]
+}
+console.log(preWord.substring(parseInt(input[n + 2].split(' ')[0])-1, parseInt(input[n + 2].split(' ')[1])))
