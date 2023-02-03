c936131a2dca09f6e4e74cd2cea9ec06a1d4ee3d
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-02-03 13:36:46 +0900

    :tada: solve baekjoon-16507

--- baekjoon/problem-16507.js
+++ baekjoon/problem-16507.js
@@ -5,21 +5,30 @@ let r = parseInt(input[0].split(' ')[0])
 let c = parseInt(input[0].split(' ')[1])
 let q = parseInt(input[0].split(' ')[2])
 let array = new Array(r)
-for (let i = 1; i <= r;i++) {
-  array[i - 1] = input[i].split(' ').map(function (v) { return parseInt(v)})
+let sumArray = new Array(r)
+for (let i = 0; i < r;i++) {
+  array[i] = input[i + 1].split(' ').map(function (v) { return parseInt(v)})
+  sumArray[i] = new Array(c)
+  for (let j = 0; j < c;j++) {
+    let sum1 = array[i][j]
+    let sum2 = i >= 1 ? sumArray[i - 1][j] : 0
+    let sum3 = j >= 1 ? sumArray[i][j - 1] : 0
+    let sum4 = i >= 1 && j >= 1 ? sumArray[i - 1][j - 1] : 0
+    sumArray[i][j] = sum1 + sum2 + sum3 - sum4
+  }
 }
 let answer = ''
 for (let i = r + 1; i <= r + q; i++) {
-  let r1 = parseInt(input[i].split(' ')[0])
-  let c1 = parseInt(input[i].split(' ')[1])
-  let r2 = parseInt(input[i].split(' ')[2])
-  let c2 = parseInt(input[i].split(' ')[3])
+  let r1 = parseInt(input[i].split(' ')[0]) - 1
+  let c1 = parseInt(input[i].split(' ')[1]) - 1
+  let r2 = parseInt(input[i].split(' ')[2]) - 1
+  let c2 = parseInt(input[i].split(' ')[3]) - 1
   let sum = 0
-  for (let j = r1; j <= r2;j++) {
-    for (let k = c1;k <= c2;k++) {
-      sum += array[j - 1][k - 1]
-    }
-  }
-  answer += (parseInt(sum / ((r2 - r1+1) * (c2 - c1+1))) + '\n')
+  let sum1 = sumArray[r2][c2]
+  let sum2 = r1 >= 1 ? sumArray[r1 - 1][c2] : 0
+  let sum3 = c1 >= 1 ? sumArray[r2][c1 - 1] : 0
+  let sum4 = r1 >= 1 && c1 >= 1 ? sumArray[r1 - 1][c1 - 1] : 0
+  sum = sum1 - sum2 - sum3 + sum4
+  answer += (parseInt(sum / ((r2 - r1 + 1) * (c2 - c1 + 1))) + '\n')
 }
 console.log(answer)
