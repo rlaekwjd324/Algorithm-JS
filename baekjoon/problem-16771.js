8095bcf35e957359097f12da3208bff911660b46
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-03-09 18:16:36 +0900

    16771 문제 이해 실패

--- baekjoon/problem-16771.js
+++ baekjoon/problem-16771.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
-let year = parseInt(input[0])
-let fridayCount = [2, 2, 1, 1, 1, 2, 1, 3, 1, 1, 2, 2, 1, 2, 1, 2, 2, ]
+let firstBuckets = input[0].split(' ').map(function (v) { return parseInt(v)})
+let secondBuckets = input[0].split(' ').map(function (v) { return parseInt(v)})
