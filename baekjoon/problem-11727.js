79ea27a00b9953cb794ebef064875570d8899f20
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-03-19 17:50:34 +0900

    :tada: solve baekjoon-11727 by dp

--- baekjoon/problem-11727.js
+++ baekjoon/problem-11727.js
@@ -1,23 +1,18 @@
 let fs = require('fs')
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
-let n = parseInt(input[0])
-let count = 0
-back(0, 1)
-function back (sum, tempCount) {
-  // 1X2
-  if (sum + 1 < n) {
-    back(sum + 1, tempCount)
-  }else if (sum + 1 === n) {
-    count += tempCount
-  }
-  // 2X1
-  if (sum + 2 < n) {
-    back(sum + 2, tempCount * 2)
-  }else if (sum + 2 === n) {
-    count += tempCount*2
-  }
-// 2X2
-//   back(sum + 2)
+let num = parseInt(input[0])
+let array = new Array(num + 1)
+for (let i = 1; i <= num; i++) {
+  array[i] = 0
 }
-console.log(count % 10007)
+array[1] = 1
+array[2] = 3
+function dp (n) {
+  if (array[n] !== 0) {
+    return array[n]
+  }
+  array[n] = (dp(n - 1) + dp(n - 2) * 2) % 10007
+  return array[n]
+}
+console.log(dp(num))
