57281b338a99847823219f438649b00006a1b707
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-03-28 18:43:44 +0900

    :tada: solve baekjoon-1932 by dp

--- baekjoon/problem-1932.js
+++ baekjoon/problem-1932.js
@@ -3,18 +3,26 @@ let fs = require('fs')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
 let n = parseInt(input[0])
 let array = new Array(n)
+let sumArray = new Array(n)
 for (let i = 0; i < n; i++) {
   array[i] = input[i + 1].split(' ').map(function (v) {return parseInt(v)})
+  sumArray[i] = new Array(i + 1)
+}
+sumArray[0][0] = array[0][0]
+for (let i = 1;i < n;i++) {
+  for (let j = 0;j < n;j++) {
+    sumArray[i][j] = array[i][j]
+    if (j === 0) {
+      sumArray[i][j] += sumArray[i - 1][0]
+    }else if (j === i) {
+      sumArray[i][j] += sumArray[i - 1][j - 1]
+    }else {
+      sumArray[i][j] += Math.max(sumArray[i - 1][j - 1], sumArray[i - 1][j])
+    }
+  }
 }
 let max = 0
-selectNumber(0, 0, array[0][0])
-function selectNumber (layer, index, sum) {
-  if (layer === n - 2) {
-    max = Math.max(max, sum + array[layer + 1][index])
-    max = Math.max(max, sum + array[layer + 1][index + 1])
-    return
-  }
-  selectNumber(layer + 1, index, sum + array[layer + 1][index])
-  selectNumber(layer + 1, index + 1, sum + array[layer + 1][index + 1])
+for (let i = 0; i < n; i++) {
+  max = Math.max(max, sumArray[n - 1][i])
 }
 console.log(max)
