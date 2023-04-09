f1912217972e866a6fa8fe4720fbe84276016c8b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-04-09 21:19:56 +0900

    :tada: solve baekjoon-27328

---
+++ baekjoon/problem-27328.js
@@ -0,0 +1,6 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = parseInt(input[0])
+let b = parseInt(input[1])
+console.log(a === b ? 0 : (a > b ? 1 : -1))
