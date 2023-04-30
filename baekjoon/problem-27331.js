3ea4398ef8ece02062f125b5f40ac37687a9f025
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-04-30 15:39:41 +0900

    :tada: solve baekjoon-27331

---
+++ baekjoon/problem-27331.js
@@ -0,0 +1,4 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+console.log(input[0] + input[1])
