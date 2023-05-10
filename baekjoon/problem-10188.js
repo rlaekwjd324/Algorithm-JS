3aaf77e6c122565df8e5e9de48df3701d46a3271
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-05-10 10:18:35 +0900

    :tada: solve baekjoon-10188

--- baekjoon/problem-10188.js
+++ baekjoon/problem-10188.js
@@ -1,1 +1,16 @@
-
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let n = parseInt(input[0])
+let answer = ''
+for (let i = 1; i <= n; i++) {
+  let x = parseInt(input[i].split(' ')[0])
+  let y = parseInt(input[i].split(' ')[1])
+  for (let j = 1; j <= y; j++) {
+    for (let k = 1; k <= x; k++) {
+      answer += 'X'
+    }
+    answer += '\n'
+  }
+  answer += '\n'
+}
+console.log(answer)
