c61750042b6c42f9f7b4b5ff9546e538bc5e57e0
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2023-05-11 13:06:22 +0900

    :tada: solve baekjoon-24084

--- baekjoon/problem-24084.js
+++ baekjoon/problem-24084.js
@@ -1,1 +1,11 @@
-
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let n = parseInt(input[0])
+let s = input[1].split('')
+let answer = ''
+for (let i = 1; i < n; i++) {
+  if (s[i] === 'J') {
+    answer += (s[i - 1] + '\n')
+  }
+}
+console.log(answer)
