803bebf16eb69d03f6c7a00bfe4193d2e0fc13df
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2023-05-12 13:15:05 +0900

    :tada: solve baekjoon-27310

--- baekjoon/problem-27310.js
+++ baekjoon/problem-27310.js
@@ -1,1 +1,4 @@
-
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let word = input[0]
+console.log((word.length + (word.split('_').length - 1) * 5 + (word.split(':').length - 1)))
