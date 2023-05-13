1fe2a861a29cb7482b003e52dfa0b2e1f4cd3583
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2023-05-13 11:51:37 +0900

    :tada: solve baekjoon-26742

--- baekjoon/problem-26742.js
+++ baekjoon/problem-26742.js
@@ -1,1 +1,12 @@
-
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let socks = input[0].split('')
+let counts = [0, 0]
+for (let i = 0; i < socks.length; i++) {
+  if (socks[i] === 'B') {
+    counts[0]++
+  }else {
+    counts[1]++
+  }
+}
+console.log((parseInt(counts[0] / 2) + parseInt(counts[1] / 2)))
