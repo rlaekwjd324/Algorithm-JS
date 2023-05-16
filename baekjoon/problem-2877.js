1e457e892914301dbf97ebad246ad1537b634b0c
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-05-16 16:44:24 +0900

    :tada: solve baekjoon-2877

--- baekjoon/problem-2877.js
+++ baekjoon/problem-2877.js
@@ -1,15 +1,24 @@
 let fs = require('fs')
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
-let k = Number(input[0])
-let count = 0
-putNumber('4')
-putNumber('7')
-function putNumber (tempNumber) {
-  if(count === k) {
-    return
-  }
-  count++
-  putNumber(tempNumber + '4')
-  putNumber(tempNumber + '7')
+let k = Number(input[0]) + 1
+let number = ''
+let n = 0
+while (k > 0) {
+  n++
+  let temp = parseInt(k / 2)
+  let leftNumber = k % 2
+  k = temp
+  number = (leftNumber === 0 ? '4' : '7') + number
 }
+console.log(number.substring(1))
+
+// 4 7 
+// 44 47 74 77 
+// 444 447 474 477 744 747 774 777 
+// 1
+// 10 11
+// 100 101 110 111
+// 1000 1001 1010 1011 1100 1101 1110 1111
+
+// 10 -> 5, 0 -> 2, 1 -> 1, 0 -> 0, 1
