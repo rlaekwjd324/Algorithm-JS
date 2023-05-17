5541f3db39df5cb12804f6a4ffffde73c7d43ecf
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-05-17 10:53:51 +0900

    :tada: solve baekjoon-27890

--- baekjoon/problem-27890.js
+++ baekjoon/problem-27890.js
@@ -3,11 +3,11 @@ let fs = require('fs')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
 let x = parseInt(input[0].split(' ')[0])
 let n = parseInt(input[0].split(' ')[1])
-for (let i = 0; i <= n; i++) {
+for (let i = 0; i < n; i++) {
   if (x % 2 === 0) {
-    x = x / 2 + 6
+    x = (x / 2) ^ 6
   }else {
-    x = x * 2 + 6
+    x = (x * 2) ^ 6
   }
 }
 console.log(x)
