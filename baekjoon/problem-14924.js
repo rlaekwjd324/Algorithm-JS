1dbe3f7d42a2e94baddc554993b413c9c9ede5cf
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-06-25 19:06:33 +0900

    :tada: solve baekjoon-14924

---
+++ baekjoon/problem-14924.js
@@ -0,0 +1,7 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = parseInt(input[0].split(' ')[0])
+let b = parseInt(input[0].split(' ')[1])
+let c = parseInt(input[0].split(' ')[2])
+console.log(c / a / 2 * b)
