d04996e64039e60e67d1970281de70892b033edf
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-07-03 14:19:17 +0900

    :tada: solve baekjoon-22193

--- baekjoon/problem-22193.js
+++ baekjoon/problem-22193.js
@@ -1,1 +1,6 @@
-
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = BigInt(input[1])
+let m = BigInt(input[2])
+console.log((n * m).toString())
--- baekjoon/problem-5928.js
+++ baekjoon/problem-5928.js
@@ -1,1 +1,21 @@
-
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let d = parseInt(input[0].split(' ')[0])
+let h = parseInt(input[0].split(' ')[1])
+let m = parseInt(input[0].split(' ')[2])
+if(m<11) {
+    h--
+    m += 60
+}
+m -=11
+if(h<11) {
+    d--
+    h+=24
+}
+h -= 11
+if(d<11) {
+    return console.log('-1')
+}
+d -= 11
+console.log(d*24*60+h*60+m)
