6b085971d340f55efbfdfeb536de205e67249439
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2023-07-08 09:43:24 +0900

    :tada: solve baekjoon-28295.js

--- baekjoon/problem-28295.js
+++ baekjoon/problem-28295.js
@@ -1,1 +1,22 @@
-
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let directions = ['N', 'E', 'S', 'W']
+let nowDirection = 0
+for (let i = 0; i < 10; i++) {
+  let direction = input[i]
+  switch (direction) {
+    case '1':
+      // 우양 우
+      nowDirection = (nowDirection + 1) % 4
+      break
+    case '2':
+      // 뒤로 돌아
+      nowDirection = (nowDirection + 2) % 4
+      break
+    case '3':
+      // 좌양 좌
+      nowDirection = (nowDirection + 3) % 4
+      break
+  }
+}
+console.log(directions[nowDirection])
