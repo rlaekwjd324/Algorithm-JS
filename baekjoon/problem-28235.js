cfa869c51f99fa1fbf7bb166cd81e6d6f238d8c3
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2023-07-10 11:55:56 +0900

    :tada: solve baekjoon-28235.js

--- baekjoon/problem-28235.js
+++ baekjoon/problem-28235.js
@@ -1,1 +1,18 @@
-
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let word = input[0]
+switch (word) {
+  case 'SONGDO':
+    console.log('HIGHSCHOOL')
+    break
+  case 'CODE':
+    console.log('MASTER')
+    break
+  case '2023':
+    console.log('0611')
+    break
+  case 'ALGORITHM':
+    console.log('CONTEST')
+    break
+}
