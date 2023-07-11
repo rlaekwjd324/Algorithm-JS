5fd09be1fcce239a27dfd28edcab4611da261f79
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2023-07-11 13:18:56 +0900

    :tada: solve baekjoon-28224.js

--- baekjoon/problem-28224.js
+++ baekjoon/problem-28224.js
@@ -1,1 +1,9 @@
-
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let t = parseInt(input[0])
+let sum = 0
+for (let i = 1; i <= t; i++) {
+  sum += (parseInt(input[i]))
+}
+console.log(sum)
