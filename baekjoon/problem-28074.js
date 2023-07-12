2ac00de2608fbdc2dbd64daac35f23c3b1b20880
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-07-12 19:57:43 +0900

    :tada: solve baekjoon-28074

--- baekjoon/problem-28074.js
+++ baekjoon/problem-28074.js
@@ -1,1 +1,11 @@
-
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let word = input[0]
+let array = 'MOBIS'.split('')
+for (let i = 0; i <array.length; i++) {
+  if(!word.includes(array[i])) {
+    return console.log('NO')
+  }
+}
+console.log('YES')
