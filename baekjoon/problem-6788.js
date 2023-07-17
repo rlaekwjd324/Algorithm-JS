e64c81b6b6d5849dd7c5a0789f90f30497847371
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-07-17 14:08:08 +0900

    :tada: solve baekjoon-6788

---
+++ baekjoon/problem-6788.js
@@ -0,0 +1,5 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let num = Number(input[0])
+console.log(parseInt((num - 1) * (num - 2) * (num - 3) / 6))
