937217ac4b96e7f66cb42164b1392f44ddd4fc01
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-09-01 10:59:12 +0900

    :tada: solve baekjoon-27327

---
+++ baekjoon/problem-27327.js
@@ -0,0 +1,5 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = Number(input[0])
+console.log(a * 24)
