aaabd9bcaa0c56d2145bd5d3f966fd2edddba905
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-09-12 11:35:53 +0900

    :tada: solve baekjoon-26711

---
+++ baekjoon/problem-26711.js
@@ -0,0 +1,6 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = BigInt(input[0])
+let b = BigInt(input[1])
+console.log((a + b).toString())
