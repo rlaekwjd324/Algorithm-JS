70d15346e28d275359a87ab1599ad02fd878f887
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-09-18 23:03:55 +0900

    :tada: solve baekjoon-20254

---
+++ baekjoon/problem-20254.js
@@ -0,0 +1,8 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = Number(input[0].split(' ')[0])
+let b = Number(input[0].split(' ')[1])
+let c = Number(input[0].split(' ')[2])
+let d = Number(input[0].split(' ')[3])
+console.log(56 * a + 24 * b + 14 * c + 6 * d)
