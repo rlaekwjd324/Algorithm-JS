499162fbd5c3d6597fc76a47fad6094c3f2c2862
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-10-02 12:40:16 +0900

    :tada: solve baekjoon-1629

--- baekjoon/problem-1629.js
+++ baekjoon/problem-1629.js
@@ -1,27 +1,18 @@
 let fs = require('fs')
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
-let a = Number(input[0].split(' ')[0])
-let b = Number(input[0].split(' ')[1])
-let c = Number(input[0].split(' ')[2])
-let res = 1
-while (b > 0){
-  if (b % 2 === 1)
-    res = (res * a) % c
-  a = ((a % c) * (a % c)) % c
-  b = b / 2
-}
-console.log(res)
+let n1 = BigInt(input[0].split(' ')[0])
+let n2 = BigInt(input[0].split(' ')[1])
+let n3 = BigInt(input[0].split(' ')[2])
+console.log(pow(n1, n2, n3).toString())
 
 function pow (a, n, c) {
-  if (n === 0)
-    return 1
-  if (n === 1)
+  if (n === 1n)
     return a % c
-
-  if (n % 2 === 0) {
-    return (pow(a, n / 2, c) * pow(a, n / 2, c)) % c
-  }else {
-    return (pow(a, n / 2 + 1, c) * pow(a, n / 2, c)) % c
-  }
+  let tmp = pow(a, n / BigInt(2), c)
+  tmp = (tmp * tmp) % c
+  if (n % BigInt(2) === 0n)
+    return tmp
+  else
+    return tmp * a % c
 }
