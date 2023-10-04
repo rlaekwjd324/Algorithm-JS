922ab1aae9cef54c99aa11f96450f02853a19dee
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-10-04 22:29:59 +0900

    :tada: solve baekjoon-25305

---
+++ baekjoon/problem-25305.js
@@ -0,0 +1,8 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = Number(input[0].split(' ')[0])
+let k = Number(input[0].split(' ')[1])
+let scores = input[1].split(' ').map(function (v) { return Number(v) })
+scores.sort(function (a, b) { return b - a})
+console.log(scores[k - 1])
