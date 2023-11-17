57b367a936de6b49a49e17b37ddc5879b8f6112b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-11-17 13:51:35 +0900

    :tada: solve baekjoon-30017

---
+++ baekjoon/problem-30017.js
@@ -0,0 +1,9 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = Number(input[0].split(' ')[0])
+let b = Number(input[0].split(' ')[1])
+if(a<=b+1) {
+  return console.log((a+a-1))
+}
+return console.log(b+b+1)
