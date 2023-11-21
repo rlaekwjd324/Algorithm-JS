0259a308e8457ac82a596ed8d00d3a277dbcf411
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2023-11-21 18:49:52 +0900

    :tada: solve baekjoon-14219

---
+++ baekjoon/problem-14219.js
@@ -0,0 +1,6 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = Number(input[0].split(' ')[0])
+let b = Number(input[0].split(' ')[1])
+console.log((a * b) % 3 === 0 ? 'YES' : 'NO')
