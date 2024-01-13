9e2fcba97c49006f4c902269612c720a87e7665a
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-01-13 15:38:49 +0900

    :tada: solve baekjoon-8932

--- baekjoon/problem-8932.js
+++ baekjoon/problem-8932.js
@@ -1,11 +1,22 @@
 let fs = require('fs')
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
-let n = BigInt(input[0].split(' ')[0])
-let m = BigInt(input[0].split(' ')[1])
-let line = input[1].split(' ')
-let number = 1
-for (let i = 0; i < n; i++) {
-  number = (BigInt(number) * (BigInt(line[i]) % BigInt(m))) % BigInt(m)
+let t = Number(input[0])
+let answer = ''
+let a = [9.23076, 1.84523, 56.0211, 4.99087, 0.188807, 15.9803, 0.11193]
+let b = [26.7, 75, 1.5, 42.5, 210, 3.8, 254]
+let c = [1.835, 1.348, 1.05, 1.81, 1.41, 1.04, 1.88]
+let type = [1, 0, 0, 1, 0, 0, 1]
+for (let i = 1; i <= t; i++) {
+  let array = input[i].split(' ').map(function (v) {return Number(v)})
+  let score = 0
+  for (let j = 0; j < 7; j++) {
+    if (type[j] === 1) {
+      score += parseInt(a[j] * Math.pow(b[j] - array[j], c[j]))
+    }else {
+      score += parseInt(a[j] * Math.pow(array[j] - b[j], c[j]))
+    }
+  }
+  answer += (score + '\n')
 }
-console.log(Number(number))
+console.log(answer)
