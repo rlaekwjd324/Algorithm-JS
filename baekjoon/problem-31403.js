4b6e6e5e5987924d6df8bfecf00002ec9a7b52b6
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-02-04 15:19:14 +0900

    :tada: solve baekjoon-31403

---
+++ baekjoon/problem-31403.js
@@ -0,0 +1,7 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let a = Number(input[0])
+let b = Number(input[1])
+let c = Number(input[2])
+console.log((a + b - c) + '\n' + (Number(a.toString() + b.toString()) - c))
