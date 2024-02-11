92666c109c858b2811a0603081295b4f6e75a57b
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-02-11 16:12:10 +0900

    :tada: solve baekjoon-17598

---
+++ baekjoon/problem-17598.js
@@ -0,0 +1,5 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+input.sort()
+console.log(input[4])
