adf41a56a8892922533bd8c63c4723aedc2b187e
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-03-05 21:41:57 +0900

    :tada: solve baekjoon-31281

---
+++ baekjoon/problem-31281.js
@@ -0,0 +1,5 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let array = input[0].split(' ').sort(function (a, b) { return Number(a) - Number(b) })
+console.log(array[1])
