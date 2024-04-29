05d0c88b1cc40b9cae85fb32742e4eea37d2f390
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-04-30 01:09:21 +0900

    :tada: solve baekjoon-31611

---
+++ baekjoon/problem-31611.js
@@ -0,0 +1,9 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = Number(input[0])
+let answer = ''
+if(n%7 === 2) {
+    return console.log(1)
+}
+console.log(0)
