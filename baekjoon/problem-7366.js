379c1dec296e011ab284ec4f924e6f1364aa4314
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-05-05 18:51:51 +0900

    solve problem-7366 temp

---
+++ baekjoon/problem-7366.js
@@ -0,0 +1,17 @@
+let fs = require('fs')
+let input = fs.readFileSync('test.txt').toString().split('\n')
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+let n = Number(input[0])
+let answer = ''
+for (let i = 1; i <= n; i++) {
+    let c = Number(input[i * 2 - 1])
+    let words = input[i * 2].split(' ')
+    let count = 0
+    for (let j = 0; j < c; j++) {
+        if(words[j] === 'sheep') {
+            count++
+        }
+    }
+    answer+= ('Case '+i+': This list contains '+count+' sheep.\n\n')
+}
+console.log(answer)
