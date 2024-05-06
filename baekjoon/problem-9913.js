0f5347f7c168ff419d866efe689f285f31561a9c
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-05-06 18:06:20 +0900

    solve baekjoon-9913 temp

---
+++ baekjoon/problem-9913.js
@@ -0,0 +1,16 @@
+let fs = require('fs')
+let input = fs.readFileSync('test.txt').toString().split('\n')
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+let n = Number(input[0])
+let array = []
+let count = []
+for (let i = 1; i<=n; i++) {
+    if(array.includes(input[i])) {
+        count[array.indexOf(input[i])]++
+    }else {
+        array.push(input[i])
+        count.push(1)
+    }
+}
+count.sort(function(a, b){return b-a})
+console.log(count[0])
