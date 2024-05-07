6f5b0dc21c5de70b6ca1910509d4e16985e8e10c
rlaekwjd324 <rew324@naver.com>
2024-05-07 12:47:30 +0900

    :tada: solve baekjoon-17042

--- baekjoon/problem-17042.js
+++ baekjoon/problem-17042.js
@@ -2,6 +2,7 @@ let fs = require('fs')
 // let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let input = fs.readFileSync('test.txt').toString().trim().split('\n')
 let m = input[0]
+let array = [m]
 let n = Number(input[1])
 let count = 1
 for (let i = 0; i < n; i++) {
@@ -9,7 +10,10 @@ for (let i = 0; i < n; i++) {
     let z2 = input[i + 2].split(' ')[1]
     if (m === z2) {
         m = z1
-        count++
+        if (!array.includes(z1)) {
+            array.push(z1)
+            count++
+        }
     }
 }
 console.log(m + '\n' + count)
