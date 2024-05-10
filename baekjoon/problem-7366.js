7727677cef1128204b3bf0bbf819f74de4933c1c
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-05-10 20:23:55 +0900

    :tada: solve baekjoon-7366

--- baekjoon/problem-7366.js
+++ baekjoon/problem-7366.js
@@ -4,14 +4,14 @@ let input = fs.readFileSync('test.txt').toString().split('\n')
 let n = Number(input[0])
 let answer = ''
 for (let i = 1; i <= n; i++) {
-    let c = Number(input[i * 2 - 1])
-    let words = input[i * 2].split(' ')
-    let count = 0
-    for (let j = 0; j < c; j++) {
-        if(words[j] === 'sheep') {
-            count++
-        }
+  let c = Number(input[i * 2 - 1])
+  let words = input[i * 2].split(' ')
+  let count = 0
+  for (let j = 0; j < c; j++) {
+    if (words[j] === 'sheep') {
+      count++
     }
-    answer+= ('Case '+i+': This list contains '+count+' sheep.\n\n')
+  }
+  answer += ('Case ' + i + ': This list contains ' + count + ' sheep.\n\n')
 }
 console.log(answer)
