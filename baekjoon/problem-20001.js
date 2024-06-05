badedb693005c6607453ff44c3a188c1d92eabb3
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-06-05 17:27:37 +0900

    :tada: solve baekjoon-20001

--- baekjoon/problem-20001.js
+++ baekjoon/problem-20001.js
@@ -1,6 +1,5 @@
 let fs = require('fs')
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let index = 1
 let problem = 0
 while (input[index] != '고무오리 디버깅 끝') {
