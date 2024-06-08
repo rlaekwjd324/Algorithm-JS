0e018d98f8401d540c6a09ee051cd898ac9aea8f
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-06-08 13:03:43 +0900

    :tada: solved baekjoon-31880

--- baekjoon/problem-31880.js
+++ baekjoon/problem-31880.js
@@ -1,6 +1,5 @@
 let fs = require('fs')
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n'
 let n = Number(input[0].split(' ')[0])
 let m = Number(input[0].split(' ')[1])
 let num1 = 0
