5d2a4eb0ee88fe0226659a2163e68df6b310a42e
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-06-09 21:23:22 +0900

    :tada: solved baekjoon-31907

--- baekjoon/problem-31907.js
+++ baekjoon/problem-31907.js
@@ -1,6 +1,5 @@
 let fs = require('fs')
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let n = Number(input[0])
 let answer = ''
 let array = ['G...'.split(''), '.I.T'.split(''), '..S.'.split('')]
