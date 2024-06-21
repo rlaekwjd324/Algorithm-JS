25ccb90ee8e83c092837a40e44ef8e2ccf68e39f
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-06-21 17:32:25 +0900

    :tada: solved baekjoon-31746

--- baekjoon/problem-31746.js
+++ baekjoon/problem-31746.js
@@ -1,6 +1,5 @@
 let fs = require('fs')
-// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
-let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let n = input[0].split('')
 let num = n[n.length - 1]
 let word = 'SciComLove'.split('')
