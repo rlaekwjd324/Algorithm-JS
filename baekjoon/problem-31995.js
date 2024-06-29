8c0cfe3fda7fa72a5c8c65b6d7b4077006238ff2
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-06-29 16:58:28 +0900

    :tada: solved baekjoon-31995

---
+++ baekjoon/problem-31995.js
@@ -0,0 +1,6 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = Number(input[0])
+let m = Number(input[1])
+console.log((n - 1) * (m - 1) * 2)
