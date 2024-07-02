1724b10ddeffbd28b9243025b59c20384a6762dc
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-07-02 22:03:46 +0900

    :tada: solve baekjoon-29701

---
+++ baekjoon/problem-29701.js
@@ -0,0 +1,57 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = Number(input[0])
+let array = {
+  'A': '.-',
+  'B': '-...',
+  'C': '-.-.',
+  'D': '-..',
+  'E': '.',
+  'F': '..-.',
+  'G': '--.',
+  'H': '....',
+  'I': '..',
+  'J': '.---',
+  'K': '-.-',
+  'L': '.-..',
+  'M': '--',
+  'N': '-.',
+  'O': '---',
+  'P': '.--.',
+  'Q': '--.-',
+  'R': '.-.',
+  'S': '...',
+  'T': '-',
+  'U': '..-',
+  'V': '...-',
+  'W': '.--',
+  'X': '-..-',
+  'Y': '-.--',
+  'Z': '--..',
+  '1': '.----',
+  '2': '..---',
+  '3': '...--',
+  '4': '....-',
+  '5': '.....',
+  '6': '-....',
+  '7': '--...',
+  '8': '---..',
+  '9': '----.',
+  '0': '-----',
+  ',': '--..--',
+  '.': '.-.-.-',
+  '?': '..--..',
+  ':': '---...',
+  '-': '-....-',
+  '@': '.--.-.'
+}
+let word = input[1].split(' ')
+let answer = ''
+for (let i = 0;i < n;i++) {
+  answer += (getKeyByValue(array, word[i]))
+}
+console.log(answer)
+function getKeyByValue (obj, value) {
+  return Object.keys(obj).find(key => obj[key] === value)
+}
