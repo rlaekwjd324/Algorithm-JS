50bcdd49c2ccaa861c98a40a66f952dbfd552bbd
rlaekwjd324 <54465767+rlaekwjd324@users.noreply.github.com>
2024-07-05 23:16:29 +0900

    :tada: solve baekjoon-31668

---
+++ baekjoon/problem-31668.js
@@ -0,0 +1,7 @@
+let fs = require('fs')
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync('test.txt').toString().trim().split('\n')
+let n = Number(input[0])
+let m = Number(input[1])
+let k = Number(input[2])
+console.log(m / n * k)
